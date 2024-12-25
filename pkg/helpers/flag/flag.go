6e1982a0226b120dae417b6c6a5962ee3b0ab0d6
ahargunyllib <billy.bpm03@gmail.com>
2024-12-25 22:25:11 +0700

    refactor: Refactor flag handling by removing unused fields and simplifying flag initialization

--- pkg/helpers/flag/flag.go
+++ pkg/helpers/flag/flag.go
@@ -5,25 +5,15 @@ import (
 )
 
 type Flag struct {
-	Fresh       bool
-	Seeder      bool
-	SeederModel string
 }
 
 var FlagVars = getFlags()
 
 func getFlags() *Flag {
-	fresh := flag.Bool("fresh", false, "Dropping all database tables before running new migration")
-	seeder := flag.Bool("seeder", false, "Inserting all seeders to database")
-	seederModel := flag.String("model", "", "Specify seeder model to run\nMust be used with flag seeder\nAvailable models can be looked up at pgsql_conn.go file func Seeder")
 
 	flag.Parse()
 
-	flag := &Flag{
-		Fresh:       *fresh,
-		Seeder:      *seeder,
-		SeederModel: *seederModel,
-	}
+	flag := &Flag{}
 
 	return flag
 }
