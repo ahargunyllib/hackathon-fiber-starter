fcbc5165cfecc5213d9975a7c2461ec9d82c23af
ahargunyllib <billy.bpm03@gmail.com>
2025-01-03 23:59:56 +0700

    feat(jwt): refactor JWT handling to use JwtInterface and JwtStruct for improved structure and functionality

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -8,18 +8,38 @@ import (
 	"github.com/google/uuid"
 )
 
+type JwtInterface interface {
+	Create(userID uuid.UUID) (string, error)
+	Decode(tokenString string, claims *Claims) error
+}
+
 type Claims struct {
 	jwt.RegisteredClaims
 	UserID uuid.UUID `json:"user_id"`
 }
 
-func Create(userID uuid.UUID) (string, error) {
+type JwtStruct struct {
+	SecretKey   string
+	ExpiredTime time.Duration
+}
+
+var Jwt = getJwt()
+
+func getJwt() JwtInterface {
+
+	return &JwtStruct{
+		SecretKey:   env.AppEnv.JwtSecretKey,
+		ExpiredTime: env.AppEnv.JwtExpTime,
+	}
+}
+
+func (j *JwtStruct) Create(userID uuid.UUID) (string, error) {
 	claims := Claims{
 		RegisteredClaims: jwt.RegisteredClaims{
 			Issuer:    "hackathon-fiber-starter",
 			Subject:   userID.String(),
 			Audience:  jwt.ClaimStrings{"hackathon-fiber-starter"},
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(env.AppEnv.JwtExpTime)),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(j.ExpiredTime)),
 			IssuedAt:  jwt.NewNumericDate(time.Now()),
 			NotBefore: jwt.NewNumericDate(time.Now()),
 			ID:        uuid.New().String(),
@@ -28,7 +48,7 @@ func Create(userID uuid.UUID) (string, error) {
 	}
 
 	unsignedJWT := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-	signedJWT, err := unsignedJWT.SignedString(env.AppEnv.JwtSecretKey)
+	signedJWT, err := unsignedJWT.SignedString(j.SecretKey)
 	if err != nil {
 		return "", err
 	}
@@ -36,7 +56,7 @@ func Create(userID uuid.UUID) (string, error) {
 	return signedJWT, nil
 }
 
-func Decode(tokenString string, claims *Claims) error {
+func (j *JwtStruct) Decode(tokenString string, claims *Claims) error {
 	token, err := jwt.ParseWithClaims(tokenString, claims, func(_ *jwt.Token) (any, error) {
 		return env.AppEnv.JwtSecretKey, nil
 	})
